069f2ca83b295cc7fd514761f47e6ccfc3a2accd
juliatdm7 <juliatdm7@gmail.com>
2024-11-27 17:25:55 +0100

    Loading data.

--- code/vertebrates_birds.R
+++ code/vertebrates_birds.R
@@ -11,3 +11,12 @@ library(colorblindr)
 
 ################################################################################
 #Birds analysis#
+vert1.dat <- read_excel("data/Arran_data1.xlsx", sheet = "Vertebrates")
+
+for (i in 1:nrow(vert1.dat)) {
+  if (vert1.dat[i, "site"] == "North") {
+    vert1.dat[i, "site"] <- "A"
+  } else if (vert1.dat[i, "site"] == "South") {
+    vert1.dat[i, "site"] <- "B"
+  }
+}
