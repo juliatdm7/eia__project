e480aa6ff21f050f884b46a13a8282e0819956d2
juliatdm7 <juliatdm7@gmail.com>
2024-11-27 16:25:07 +0100

    Incorporating bog data.

--- code/terrestrial_invertebrates.R
+++ code/terrestrial_invertebrates.R
@@ -84,8 +84,70 @@ shannon.alpha.dat.B <- diversity(dat.B.ab, index = "shannon") # site B
 simpson.alpha.dat.A <- diversity(dat.A.ab, index = "simpson") # site A
 simpson.alpha.dat.B <- diversity(dat.B.ab, index = "simpson") # site B
 
+# Converting abundance data to presence/absence data in order to calculate richness and Soerensen and Jaccard indices:
+dat.A.pa <- ifelse(dat.A.ab > 0, 1, 0) # site A
+dat.B.pa <- ifelse(dat.B.ab > 0, 1, 0) # site B
+
+#Computing order richness
+richness_ti_A <- ncol(dat.A.pa) 
+richness_ti_B <- ncol(dat.B.pa)
+
+richness_ti <- data.frame(
+  Site = c("A", "B"),
+  Richness = c(richness_ti_A, richness_ti_B)
+)
+
 # Gamma Diversity for site A
 combined.dat.A.ab <- colSums(dat.A.ab) 
 gamma.richness.dat.A <- specnumber(combined.dat.A.ab)
+
+
+##########################INCLUDING BOG TERRESTRIAL SAMPLING#############################################
+#Run this to check for bog instead of coding lines between 15 and 79
+
+bog <- read_excel("data/Arran_data1.xlsx", sheet = "BOG")
+ti.bog <- bog[which(bog$samplingProtocol=="Sweep netting"),]
+orderNA <- c(which(is.na(ti.bog$order))) ## no rows have NA’s for Order
+ti.bog.dat <- ti.bog
+for (i in 1:nrow(ti.bog.dat)) {
+  if (ti.bog.dat[i, "site"] == "North") {
+    ti.bog.dat[i, "site"] <- "A"
+  } else if (ti.bog.dat[i, "site"] == "South") {
+    ti.bog.dat[i, "site"] <- "B"
+  }
+}
+ti.bog.dat$new_locationID <- "Bog"
+dat.A.prov <- ti.data[which(ti.data$site=="A"),]
+dat.B.prov <- ti.data[which(ti.data$site=="B"),]
+dat.it <- rbind(dat.A.prov, dat.B.prov, ti.bog.dat)
+dat.A <- dat.it[which(dat.it$site=="A"),]
+dat.B <- dat.it[which(dat.it$site=="B"),]
+
+#Site A
+dat.it.A.red <- dat.A[,c("order","new_locationID","individualCount")]
+dat.A.redagg <- dat.it.A.red %>%
+  group_by(order, new_locationID) %>%
+  summarise(total_individuals = sum(individualCount), .groups = "drop")
+dat.A.ab <- dat.A.redagg %>%  #  here we're passing the dat.soc.red data frame into the next function using the operator pipe (%>%)
+  pivot_wider(names_from=order,values_from=c(total_individuals)) # the pivot_wider() function of tidyverse converts data from a long format to a wide format, creating a new dataframe in the values under "species" become columns, the "islands" become rows and the cells are filled with the corresponding "presence" values 
+list0 <- as.list(rep(0,ncol(dat.A.ab))) # here we create a list with as many 0s as there are columns in dat.soc.pa
+names(list0) <- names(dat.A.ab) # we assign the species names to this list of 0s
+dat.A.ab <- as.data.frame(dat.A.ab %>% replace_na(list0)) # here we replace all NAs with 0s for all species in dat.soc.pa
+row.names(dat.A.ab) <- dat.A.ab$new_locationID # here we give to each row the name of its corresponding island
+dat.A.ab <- dat.A.ab[,-1]
+
+##SiteB##
+dat.B.red <- dat.B[,c("order","new_locationID","individualCount")]
+dat.B.redagg <- dat.B.red %>%
+  group_by(order, new_locationID) %>%
+  summarise(total_individuals = sum(individualCount), .groups = "drop")
+dat.B.ab <- dat.B.redagg %>%  #  here we're passing the dat.soc.red data frame into the next function using the operator pipe (%>%)
+  pivot_wider(names_from=order,values_from=c(total_individuals)) # the pivot_wider() function of tidyverse converts data from a long format to a wide format, creating a new dataframe in the values under "species" become columns, the "islands" become rows and the cells are filled with the corresponding "presence" values 
+list0 <- as.list(rep(0,ncol(dat.B.ab))) # here we create a list with as many 0s as there are columns in dat.soc.pa
+names(list0) <- names(dat.B.ab) # we assign the species names to this list of 0s
+dat.B.ab <- as.data.frame(dat.B.ab %>% replace_na(list0)) # here we replace all NAs with 0s for all species in dat.soc.pa
+row.names(dat.B.ab) <- dat.B.ab$new_locationID # here we give to each row the name of its corresponding island
+dat.B.ab <- dat.B.ab[,-1]
+###########################################################################################################################################################################################################################################################################
 
 
