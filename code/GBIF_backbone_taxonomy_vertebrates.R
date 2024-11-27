f90b6a22e2d7c5df17d7c521cdc85488077b5812
juliatdm7 <juliatdm7@gmail.com>
2024-11-27 18:18:26 +0100

    Doing stuff.

--- code/GBIF_backbone_taxonomy_vertebrates.R
+++ code/GBIF_backbone_taxonomy_vertebrates.R
@@ -6,3 +6,17 @@ library(readxl)
 library(rgbif)
 bird.vert.north.south <- read_excel("data/EIA_data.xlsx", sheet = "Birds")
 View(bird.vert.north.south)
+
+table <- data.frame(scientificName=as.character(unique(bird.vert.north.south$scientificName)))
+write.csv(table, 'data/Birds_North_South.csv', row.names = F)
+spp <- unique(bird.vert.north.south$scientificName)
+n_spp <- length(spp)
+
+# Initialize the data frames for a species that is sure to be in the taxonomy
+spp.check.ok <- name_backbone("Ardea cinerea", verbose = TRUE, strict = TRUE)
+spp.check.ok <- spp.check.ok[-c(1:nrow(spp.check.ok)), ]  # Remove all the rows
+spp.check.ok <- spp.check.ok[,-which(names(spp.check.ok) %in% c("acceptedUsageKey","family","genus","familyKey","genusKey","class","classKey"))]
+
+# Initialize a data frame for a bad word (not a species name)
+spp.check.bad <- name_backbone("xxx", verbose = TRUE, strict = TRUE)
+spp.check.bad <- spp.check.bad[-1, ]  # Remove this row
