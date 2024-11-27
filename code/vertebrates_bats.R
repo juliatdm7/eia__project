64caca1cb6f65a60d2f58e6869dd97f50f741f27
juliatdm7 <juliatdm7@gmail.com>
2024-11-27 17:32:29 +0100

    Editing data.

--- code/vertebrates_bats.R
+++ code/vertebrates_bats.R
@@ -26,3 +26,16 @@ for (i in 1:nrow(vert2.dat)) {
 
 bat.dat <- vert2.dat[which(vert2.dat$samplingProtocol=="Audiomoths"),]
 
+bat.dat.red <- bat.dat[,c("vernacularName","site","individualCount")]
+unique(bat.dat.red$vernacularName) # Five bats species (or genus) are shared among sites
+bat.dat.redagg <- bat.dat.red %>%
+  group_by(vernacularName, site) %>%
+  summarise(total_calls = sum(individualCount), .groups = "drop") # We aggregate difference samples of the same Orders
+bat.dat.ab <- bat.dat.redagg %>%  #  here we're passing the dat.soc.red data frame into the next function using the operator pipe (%>%)
+  pivot_wider(names_from=vernacularName,values_from=c(total_calls)) # the pivot_wider() function of tidyverse converts data from a long format to a wide format, creating a new dataframe in the values under "species" become columns, the "islands" become rows and the cells are filled with the corresponding "presence" values 
+list0 <- as.list(rep(0,ncol(bat.dat.ab))) # here we create a list with as many 0s as there are columns in dat.soc.pa
+names(list0) <- names(bat.dat.ab) # we assign the species names to this list of 0s
+bat.dat.ab <- as.data.frame(bat.dat.ab %>% replace_na(list0)) # here we replace all NAs with 0s for all species in dat.soc.pa
+row.names(bat.dat.ab) <- bat.dat.ab$site # here we give to each row the name of its corresponding island
+bat.dat.ab <- bat.dat.ab[,-1]
+
