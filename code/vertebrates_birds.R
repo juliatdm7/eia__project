6c0cbe4409e3bda0c0eab44ed9762c51e13488bd
juliatdm7 <juliatdm7@gmail.com>
2024-11-28 03:37:37 +0100

    Attempt 1 at classifying bird observations by sampling sections.

--- code/vertebrates_birds.R
+++ code/vertebrates_birds.R
@@ -43,8 +43,8 @@ ncol(bird.dat.pa) # The total number of species identified across sites is 21
 
 #Calculating species richness between sites
 
-richness_bird_A <- rowSums(bird.dat.pa)[1] # 12 out of 21 species found in site A
-richness_bird_B <- rowSums(bird.dat.pa)[2] # 19 out of 21 species found in site B
+richness_bird_A <- rowSums(bird.dat.pa)[2] # 19 out of 21 species found in site A
+richness_bird_B <- rowSums(bird.dat.pa)[1] # 12 out of 21 species found in site B
 
 richness_bird <- data.frame(
   Site = c("A", "B"),
@@ -57,8 +57,9 @@ richness_bird_plot <- ggplot(richness_bird, aes(x = Site, y = Richness, fill = S
   labs(title = "Species richness", x = "Site", y = "Nr of Species") +
   scale_fill_manual(values = c("A" = "#0073e6", "B" = "#f194b8")) +
   scale_y_continuous(expand = expansion(mult = c(0, 0.2))) +
+  theme_bw() +
   theme(
-    plot.title = element_text(hjust = 0.5, margin = margin(t = 5, b = 40), size = 16),  
+    plot.title = element_text(hjust = 0.5, margin = margin(t = 5, b = 20), size = 16),  
     axis.title = element_text(size = 14),  
     axis.text = element_text(size = 12),  
     strip.text = element_text(size = 12))
@@ -66,3 +67,12 @@ ggsave("figures/vertebrates_birds/Birds_richness.png")
 cvd_grid(richness_bird_plot)
 
 # Maybe I should consider differentiating between top, middle, and bottom?
+
+#Option  1 of sections
+bird.dat$new_locationID <- "0"
+bird.dat[1:8,32] <- "Bog"
+bird.dat[9:15,32] <- "Middle"
+bird.dat[16:19,32] <- "Top"
+bird.dat[20:31,32] <- "Bottom"
+bird.dat[32:38,32] <- "Middle"
+bird.dat[c(39,40),32] <- "Top"
