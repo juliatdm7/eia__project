cd5cf7ec1f4c65f6f4fa75262b7565cc8a09e1f1
juliatdm7 <juliatdm7@gmail.com>
2024-11-29 14:12:21 +0100

    Minor code and annotations modifications.

--- code/terrestrial_invertebrates.R
+++ code/terrestrial_invertebrates.R
@@ -11,6 +11,7 @@ library(colorblindr)
 
 ################################################################################
 #Terrestrial invertebrates analysis#
+rm(list=ls()) # Not necessary, but advised
 
 north_ti <- read_excel("data/Arran_data1.xlsx", sheet = "North side invert transects")
 south_ti <- read_excel("data/Arran_data1.xlsx", sheet = "South side invert transects")
@@ -49,7 +50,7 @@ View(ti.data)
 dat.A <- ti.data[which(ti.data$site=="A"),]
 dat.B <- ti.data[which(ti.data$site=="B"),]
 
-# To compute community patterns, we need site by species data frames for each study site
+# To compute community patterns, we need site by order data frames for each study site
 # In this case, we want to compare diversity not only between study sites but also between "heights" (Top, Middle, Bottom), so we'll consider those as "site" in our site by species matrix
 
 ##SiteA##
@@ -57,25 +58,25 @@ dat.A.red <- dat.A[,c("order","new_locationID","individualCount")]
 dat.A.redagg <- dat.A.red %>%
   group_by(order, new_locationID) %>%
   summarise(total_individuals = sum(individualCount), .groups = "drop")
-dat.A.ab <- dat.A.redagg %>%  #  here we're passing the dat.soc.red data frame into the next function using the operator pipe (%>%)
-  pivot_wider(names_from=order,values_from=c(total_individuals)) # the pivot_wider() function of tidyverse converts data from a long format to a wide format, creating a new dataframe in the values under "species" become columns, the "islands" become rows and the cells are filled with the corresponding "presence" values 
-list0 <- as.list(rep(0,ncol(dat.A.ab))) # here we create a list with as many 0s as there are columns in dat.soc.pa
-names(list0) <- names(dat.A.ab) # we assign the species names to this list of 0s
-dat.A.ab <- as.data.frame(dat.A.ab %>% replace_na(list0)) # here we replace all NAs with 0s for all species in dat.soc.pa
-row.names(dat.A.ab) <- dat.A.ab$new_locationID # here we give to each row the name of its corresponding island
-dat.A.ab <- dat.A.ab[,-1] # we remove the island column because we don't need it anymore, as we have given island names to all rows
+dat.A.ab <- dat.A.redagg %>%  
+  pivot_wider(names_from=order,values_from=c(total_individuals)) 
+list0 <- as.list(rep(0,ncol(dat.A.ab))) 
+names(list0) <- names(dat.A.ab) 
+dat.A.ab <- as.data.frame(dat.A.ab %>% replace_na(list0)) 
+row.names(dat.A.ab) <- dat.A.ab$new_locationID 
+dat.A.ab <- dat.A.ab[,-1] 
 
 ##SiteB##
 dat.B.red <- dat.B[,c("order","new_locationID","individualCount")]
 dat.B.redagg <- dat.B.red %>%
   group_by(order, new_locationID) %>%
   summarise(total_individuals = sum(individualCount), .groups = "drop")
-dat.B.ab <- dat.B.redagg %>%  #  here we're passing the dat.soc.red data frame into the next function using the operator pipe (%>%)
-  pivot_wider(names_from=order,values_from=c(total_individuals)) # the pivot_wider() function of tidyverse converts data from a long format to a wide format, creating a new dataframe in the values under "species" become columns, the "islands" become rows and the cells are filled with the corresponding "presence" values 
-list0 <- as.list(rep(0,ncol(dat.B.ab))) # here we create a list with as many 0s as there are columns in dat.soc.pa
-names(list0) <- names(dat.B.ab) # we assign the species names to this list of 0s
-dat.B.ab <- as.data.frame(dat.B.ab %>% replace_na(list0)) # here we replace all NAs with 0s for all species in dat.soc.pa
-row.names(dat.B.ab) <- dat.B.ab$new_locationID # here we give to each row the name of its corresponding island
+dat.B.ab <- dat.B.redagg %>%  
+  pivot_wider(names_from=order,values_from=c(total_individuals)) 
+list0 <- as.list(rep(0,ncol(dat.B.ab))) 
+names(list0) <- names(dat.B.ab) 
+dat.B.ab <- as.data.frame(dat.B.ab %>% replace_na(list0)) 
+row.names(dat.B.ab) <- dat.B.ab$new_locationID 
 dat.B.ab <- dat.B.ab[,-1]
 
 # Computing Shannon's and Simpson's diversity indices:
@@ -92,10 +93,10 @@ dat.B.pa <- ifelse(dat.B.ab > 0, 1, 0) # site B
 richness_ti_A <- ncol(dat.A.pa)  # 6 orders present in Site A
 richness_ti_B <- ncol(dat.B.pa)  # 9 orders present in Site B
 
-#richness_ti <- data.frame(
-#  Site = c("A", "B"),
-#  Richness = c(richness_ti_A, richness_ti_B)
-#)
+richness_ti <- data.frame(
+  Site = c("A", "B"),
+  Richness = c(richness_ti_A, richness_ti_B)
+)
 
 # Gamma Diversity for site A
 combined.dat.A.ab <- colSums(dat.A.ab) 
@@ -435,7 +436,6 @@ length(which(ti.raw.data$site=="B")) #comprobation
 #We're gonna perform analysis for Order level, so we'll erase rows with NAs for rows:
 orderNA <- c(which(is.na(ti.raw.data$order))) ##which species have NA’s
 ti.data <- ti.raw.data[-orderNA,]
-View(ti.data)
 
 # To compute community patterns, we need site by species data frames for each study site
 # In this case, we want to compare diversity not only between study sites but also between "heights" (Top, Middle, Bottom), so we'll consider those as "site" in our site by species matrix
@@ -564,7 +564,6 @@ length(which(ti.raw.data$site=="B")) #comprobation
 #We're gonna perform analysis for Order level, so we'll erase rows with NAs for rows:
 orderNA <- c(which(is.na(ti.raw.data$order))) ##which species have NA’s
 ti.data <- ti.raw.data[-orderNA,]
-View(ti.data)
 
 # To compute community patterns, we need site by species data frames for each study site
 # In this case, we want to compare diversity not only between study sites but also between "heights" (Top, Middle, Bottom), so we'll consider those as "site" in our site by species matrix
